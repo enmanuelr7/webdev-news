0e0d7b6ca459461000ad2fd48d7462051e6faeb3
Enmanuel Ramirez <enmanuelr7@gmail.com>
2021-11-20 15:17:02 -0300

    nav component and link

---
+++ components/Nav.js
@@ -0,0 +1,19 @@
+import styles from "../styles/nav.module.css";
+import Link from "next/link";
+
+const Nav = () => {
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
