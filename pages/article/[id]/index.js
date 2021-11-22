432097152c6aa0f7858edb5e9142d96b716be652
Enmanuel Ramirez <enmanuelr7@gmail.com>
2021-11-22 08:06:32 -0300

    nested routing

---
+++ pages/article/[id]/index.js
@@ -0,0 +1,55 @@
+import Link from "next/link";
+// import { useRouter } from "next/router";
+
+const article = ({ article }) => {
+  // const {
+  //   query: { id },
+  // } = useRouter();
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href="/">Go Back</Link>
+    </>
+  );
+};
+
+// export const getServerSideProps = async (context) => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+//   );
+//   const article = await res.json();
+//   return {
+//     props: {
+//       article,
+//     },
+//   };
+// };
+
+export const getStaticProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+  const article = await res.json();
+  return {
+    props: {
+      article,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const articles = await res.json();
+  const paths = articles.map((article) => ({
+    params: { id: article.id.toString() },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export default article;
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import ArticleList from "../components/ArticleList";
 
 export default function Home({ articles }) {
-  console.log(articles);
   return (
     <div>
       <Head>
